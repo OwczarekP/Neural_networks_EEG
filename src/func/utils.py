40f30769167e4f5c094faf4c23b0e8810e47dcbf
Owczarek <patrycja.k.owczarek@gmail.com>
2022-07-04 11:12:18 +0200

    add evaluation functions

--- src/func/utils.py
+++ src/func/utils.py
@@ -7,3 +7,24 @@ def normalize_data(input_data: np.ndarray):
     norm = np.linalg.norm(input_data)
     normalized = input_data/norm
     return normalized
+
+
+def get_train_plot(model_history):
+    plt.plot(model_history.history['accuracy'])
+    plt.plot(model_history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.ylim(0, 1.1)
+    plt.legend(['accuracy', 'val_acc'], loc='lower right')
+    plt.show()
+
+
+def get_test_acc(model, X_test, Y_test):
+    pred = model.predict(X_test)
+    predict_classes = np.argmax(pred,axis=1)
+    expected_classes = np.argmax(Y_test,axis=1)
+    print(expected_classes.shape)
+    print(predict_classes.shape)
+    correct = accuracy_score(expected_classes,predict_classes)
+    print(f"Training Accuracy: {correct}")
